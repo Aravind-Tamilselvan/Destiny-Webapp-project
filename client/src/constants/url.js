d4ae3af5a309343249321e32bca94d0670ab2696
Aravind <aravindtamilselvan7@gmail.com>
2025-04-23 12:19:36 +0530

    Update url.js

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -1,2 +1,2 @@
-//export const baseUrl = import.meta.mode === "development" ? "http://localhost:7000" : "https://goto-app-aravind.onrender.com"
-export const baseUrl = "http://localhost:7000"
+export const baseUrl = import.meta.mode === "development" ? "http://localhost:7000" : "https://goto-app-aravind.onrender.com"
+//export const baseUrl = "http://localhost:7000"
