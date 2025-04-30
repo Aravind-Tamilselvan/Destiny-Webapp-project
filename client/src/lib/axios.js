63592b12d33922398c56bb7d4e9accb092d8b5e2
Aravind-Tamilselvan <aravindtamilselvan7@gmail.com>
2025-04-30 13:13:08 +0530

    package json update

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.mode === "development" ? "http://localhost:7000/api" : "https://goto-app-aravind.onrender.com/api",
+	baseURL: import.meta.mode === "development" ? "http://localhost:7000/api" : window.location.origin + "/api",
 	withCredentials: true, // send cookies to the server
 });
 
