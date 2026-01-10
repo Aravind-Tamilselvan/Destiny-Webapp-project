f60a14623a5550f274adf4041b5628bcff44e593
AswanthTamilselvan <aswantht11@gmail.com>
2026-01-10 13:39:26 +0530

    Added requirements.txt

--- client/src/hooks/useChatMutation.jsx
+++ client/src/hooks/useChatMutation.jsx
@@ -2,7 +2,7 @@ import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 const sendMessage = async (message) => {
-  const res = await axios.post("http://localhost:7000/api/chat/chat", {
+  const res = await axios.post(import.meta.mode === 'development' ? "http://localhost:7000/api/chat/chat" : "https://chatbot-backend.onrender.com/api/chat/chat", {
     message,
   });
   return res.data;
--- server/index.js
+++ server/index.js
@@ -24,8 +24,7 @@ app.use(express.json({
 }))
 app.use(cookieParser())
 app.use(cors({
-    origin : "http://localhost:5173",
-    credentials: true
+    origin : "*",
 }))
 app.use(express.urlencoded({ extended: true }));
 
